944f4846918f512f5c775aff0c1053d467b5f78a
Tony DiNitto <think41c@gmail.com>
2015-03-24 15:08:25 -0600

    Some tests passing. Need new logic on style of roman numberals.

--- roman.rb
+++ roman.rb
@@ -7,49 +7,33 @@ class Fixnum
     self > 1000 
     m_num = self / 1000 
     remainder = self - (m_num * 1000)
-    p remainder 
-    p m = "M"*m_num
-
-    puts "\n"
+    m = "M"*m_num
 
     d_num = remainder / 500 
     remainder = remainder - (d_num * 500)
-    p remainder
-    p d = "D"*d_num
-
-    puts "\n"
+    d = "D"*d_num
 
     c_num = remainder / 100
     remainder = remainder - (c_num * 100)
-    p remainder 
-    puts "\n"
-    p c = "C"*c_num
+    c = "C"*c_num
     
     l_num = remainder / 50 
     remainder = remainder - (l_num * 50)
-    p remainder
-    puts "\n"
-    p l = "L"*l_num
+    l = "L"*l_num
 
     x_num = remainder / 10 
     remainder = remainder - (x_num * 10)
-    p remainder
-    puts "\n"
-    p x = "X"*x_num
+    x = "X"*x_num
 
     v_num = remainder / 5 
     remainder = remainder - (v_num * 5)
-    p remainder
-    puts "\n"
-    p v = "V"*v_num
+    v = "V"*v_num
     
     i_num = remainder / 1
     remainder = remainder - (i_num * 1)
-    p remainder
-    puts "\n"
-    p i = "I"*i_num
+    i = "I"*i_num
 
-    puts "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
+    "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
 
     # V,5
     # X,10
@@ -63,6 +47,6 @@ end
 
 
 
-1132.to_roman
+# 23.to_roman
 
 # p "H"*3
--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -8,12 +8,10 @@ class RomanTest < Minitest::Test
   end
 
   def test_2
-    skip
     assert_equal 'II', 2.to_roman
   end
 
   def test_3
-    skip
     assert_equal 'III', 3.to_roman
   end
 
