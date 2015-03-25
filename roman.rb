1c9da1cc125a3045baef78c9eb437aab18463f70
Tony DiNitto <think41c@gmail.com>
2015-03-25 17:55:03 -0600

    Beginning to implement initial logic.

--- roman.rb
+++ roman.rb
@@ -2,58 +2,47 @@ class Fixnum
   
   def to_roman
 
-    # Counts up by 3 of the smaller numeral, and then precedes one numeral by the larger numeral.
-    # VIII = 8 
-    # IX   = 9 
-    # X    = 10 
+    num_length = self.to_s.length
 
-    # XCVIII = 98 
-    # XCIX   = 99
-    # XC     = 90, IX = 9 
+    # This implementation does NOT allow for proper 'subtraction' in roman numerals, but counts up fine. 
+    # self > 1000 
+    # m_num = self / 1000 
+    # remainder = self - (m_num * 1000)
+    # m = "M"*m_num
 
+    # d_num = remainder / 500 
+    # remainder = remainder - (d_num * 500)
+    # d = "D"*d_num
 
-    # 890 - DCCCXC   
-    # DCCC = 800 , XC = 90
-
-
-    self > 1000 
-    m_num = self / 1000 
-    remainder = self - (m_num * 1000)
-    m = "M"*m_num
-
-    d_num = remainder / 500 
-    remainder = remainder - (d_num * 500)
-    d = "D"*d_num
-
-    c_num = remainder / 100
-    remainder = remainder - (c_num * 100)
-    c = "C"*c_num
+    # c_num = remainder / 100
+    # remainder = remainder - (c_num * 100)
+    # c = "C"*c_num
     
-    l_num = remainder / 50 
-    remainder = remainder - (l_num * 50)
-    l = "L"*l_num
+    # l_num = remainder / 50 
+    # remainder = remainder - (l_num * 50)
+    # l = "L"*l_num
 
-    x_num = remainder / 10 
-    remainder = remainder - (x_num * 10)
-    x = "X"*x_num
+    # x_num = remainder / 10 
+    # remainder = remainder - (x_num * 10)
+    # x = "X"*x_num
 
-    v_num = remainder / 5 
-    remainder = remainder - (v_num * 5)
+    # v_num = remainder / 5 
+    # remainder = remainder - (v_num * 5)
     
     
-    i_num = remainder / 1
-    remainder = remainder - (i_num * 1)
-    if i_num == 4 then 
-      # p i_num
-      i_num = 1
-      # p v_num
-      v_num = v_num + 1 
-    end
+    # i_num = remainder / 1
+    # remainder = remainder - (i_num * 1)
+    # if i_num == 4 then 
+    #   # p i_num
+    #   i_num = 1
+    #   # p v_num
+    #   v_num = v_num + 1 
+    # end
 
-    v = "V"*v_num
-    i = "I"*i_num
+    # v = "V"*v_num
+    # i = "I"*i_num
 
-    "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
+    # "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
 
     # *V,5
     # *X,10
@@ -63,6 +52,7 @@ class Fixnum
     # *M,1000
   end
 end
+
 
 #  1 5   8   7 
 #  M D LXXX VII
