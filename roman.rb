fc14389d65d0d60f00124cc8d5f8c92bb784dae0
Tony DiNitto <think41c@gmail.com>
2015-03-25 19:02:58 -0600

    Full logic implemented. Refactoring needed.

--- roman.rb
+++ roman.rb
@@ -1,10 +1,5 @@
-class Fixnum
-  
-  def to_roman
-
-    num_length = self.to_s.length
-
-    # This implementation does NOT allow for proper 'subtraction' in roman numerals, but counts up fine. 
+######################
+# This implementation does NOT allow for proper 'subtraction' in roman numerals, but counts up fine. 
     # self > 1000 
     # m_num = self / 1000 
     # remainder = self - (m_num * 1000)
@@ -44,28 +39,64 @@ class Fixnum
 
     # "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
 
-    # *V,5
-    # *X,10
-    # *L,50
-    # *C,100
-    # *D,500
-    # *M,1000
+   
+######################################    
+
+
+
+class Fixnum
+  def to_roman
+    answer = []
+    num_length = self.to_s.length
+    num_string = self.to_s
+
+
+      case num_string[-4]
+      when "1" then answer << "M"
+      when "2" then answer << "MM"
+      when "3" then answer << "MMM"
+      when "4" then answer << "MMMM"
+      end
+
+      case num_string[-3]
+      when "1" then answer << "C"
+      when "2" then answer << "CC"
+      when "3" then answer << "CCC"
+      when "4" then answer << "CD"
+      when "5" then answer << "D"
+      when "6" then answer << "DC"
+      when "7" then answer << "DCC"
+      when "8" then answer << "DCCC"
+      when "9" then answer << "CM"
+      end
+
+      case num_string[-2]
+      when "1" then answer << "X"
+      when "2" then answer << "XII"
+      when "3" then answer << "XIII"
+      when "4" then answer << "XL"
+      when "5" then answer << "L"
+      when "6" then answer << "LX"
+      when "7" then answer << "LXX"
+      when "8" then answer << "LXXX"
+      when "9" then answer << "XC"
+      end
+
+      case num_string[-1]
+      when "1" then answer << "I"
+      when "2" then answer << "II"
+      when "3" then answer << "III"
+      when "4" then answer << "IV"
+      when "5" then answer << "V"
+      when "6" then answer << "VI"
+      when "7" then answer << "VII"
+      when "8" then answer << "VIII"
+      when "9" then answer << "IX"
+      end
+
+
+    answer.join
   end
 end
 
-
-#  1 5   8   7 
-#  M D LXXX VII
-
-#   8    9    2 
-# DCCC  XC    II 
-
-#   2    3    4
-#  CC   XXX   IV 
-
-#   9    8    9 
-#  CM   LXXX  IX
-
-# Think about the placement of the digit and compute each number separately. 
-
-p 14.to_roman
+p 982.to_roman
